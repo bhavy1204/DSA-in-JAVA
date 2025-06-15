70d064347dd1baaeef0ae24e26838be167ad870b
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-15 19:07:56 +0530

    complete max and min subarray from brute and prefix approach

---
+++ Arrays/MaxSumOfSubArray.java
@@ -0,0 +1,66 @@
+public class MaxSumOfSubArray {
+    public static void bruteForce(int arr[]) {
+
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+        int cnt = 0, sum = 0, minStart = -1, minEnd = -1, maxStart = -1, maxEnd = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                    sum += arr[k];
+                    if (sum > maxSum) {
+                        maxStart = i;
+                        maxEnd = j;
+                        maxSum = sum;
+                    }
+                    if (sum < minSum) {
+                        minStart = i;
+                        minEnd = j;
+                        minSum = sum;
+                    }
+                }
+                cnt++;
+                sum = 0;
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+        System.out.println("Total sub arrays : " + cnt);
+        System.out.println("\nMin sum : " + minSum);
+        System.out.println("\nMin sub array :- ");
+        for (int i = minStart; i <= minEnd; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("\nMax sum : " + maxSum);
+        for (int i = maxStart; i <= maxEnd; i++)
+            System.out.print(arr[i] + " ");
+
+    }
+
+    public static void prefixSum(int arr[]) {
+        int currSUm = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1]+arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                currSUm = i==0? prefix[j]: prefix[j] - prefix[i-1];
+                if(maxSum<currSUm)
+                    maxSum=currSUm;
+            }
+        }
+
+        System.out.println("MAximum sum : "+maxSum);
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 2, 4, 6, 8, 10 };
+        // bruteForce(num);
+        prefixSum(num);
+    }
+}
--- Arrays/SubArray.java
+++ Arrays/SubArray.java
@@ -1,16 +1,38 @@
 public class SubArray {
     public static void subArray(int arr[]) {
-        int cnt = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+        int cnt = 0,sum=0,minStart=-1,minEnd=-1,maxStart=-1,maxEnd=-1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-                for (int k = i; k <= j; k++)
+                for (int k = i; k <= j; k++){
                     System.out.print(arr[k] + " ");
+                    sum += arr[k];
+                    if(sum>maxSum){
+                        maxStart = i;
+                        maxEnd=j;
+                        maxSum=sum;
+                    }
+                    if(sum<minSum){
+                        minStart = i;
+                        minEnd=j;
+                        minSum=sum;
+                    }
+                }
                 cnt++;
+                sum=0;
                 System.out.println("");
             }
             System.out.println("");
         }
         System.out.println("Total sub arrays : "+cnt);
+        System.out.println("Min sum : "+minSum);
+        System.out.println("Min sub array :- ");
+        for (int i = minStart; i < minEnd; i++) 
+            System.out.print(arr[i]+" ");
+        System.out.println("Max sum : "+maxSum);
+        for (int i = maxStart; i < maxEnd; i++) 
+            System.out.print(arr[i]+" ");
     }
 
     public static void main(String args[]) {
