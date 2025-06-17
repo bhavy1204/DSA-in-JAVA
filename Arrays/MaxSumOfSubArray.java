cffabcef6a6233c4362a853cfe5254c8d579594c
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-17 08:47:09 +0530

    complete kadanes algorithm

--- Arrays/MaxSumOfSubArray.java
+++ Arrays/MaxSumOfSubArray.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MaxSumOfSubArray {
     public static void bruteForce(int arr[]) {
 
@@ -44,23 +47,45 @@ public class MaxSumOfSubArray {
         int prefix[] = new int[arr.length];
         prefix[0] = arr[0];
         for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i-1]+arr[i];
+            prefix[i] = prefix[i - 1] + arr[i];
         }
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                currSUm = i==0? prefix[j]: prefix[j] - prefix[i-1];
-                if(maxSum<currSUm)
-                    maxSum=currSUm;
+            for (int j = i; j < arr.length; j++) {
+                currSUm = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (maxSum < currSUm)
+                    maxSum = currSUm;
             }
         }
 
-        System.out.println("MAximum sum : "+maxSum);
+        System.out.println("MAximum sum : " + maxSum);
+    }
+
+    public static void kadaneAlgo(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int maxNeg = Integer.MIN_VALUE;
+        boolean allNeg=true;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>=0){
+                allNeg = false;
+                break;
+            }
+            maxNeg = Math.max(maxNeg, arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            currSum = currSum + arr[i];
+            if (currSum < 0)
+                currSum = 0;
+            maxSum = Math.max(maxSum, currSum);
+        } 
+        System.out.println("Maximum sum : " + (allNeg? maxNeg: maxSum));
     }
 
     public static void main(String[] args) {
-        int num[] = { 2, 4, 6, 8, 10 };
+        int num[] = { -2, -3, -4, -1, 5, -3 };
         // bruteForce(num);
-        prefixSum(num);
+        // prefixSum(num);
+        kadaneAlgo(num);
     }
 }
