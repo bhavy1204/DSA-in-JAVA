366f6731e5b27393fbe676caa458992f3f5bcb8b
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-18 21:40:42 +0530

    complete insertion sort

---
+++ Basic Sorting algorithms/InsertionSort.java
@@ -0,0 +1,23 @@
+public class InsertionSort {
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 6, 2, 9, 5, 8, 4, 7 };
+        insertionSort(arr);
+    }
+}
