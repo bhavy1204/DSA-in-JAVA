dcb49fdbba40463b8de6facb536965c36cfee814
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-23 19:28:16 +0530

    complete all basic pattern qquestionss

---
+++ Patterns/Invertedtriangle.java
@@ -0,0 +1,10 @@
+public class Invertedtriangle {
+    public static void main(String[] args) {
+        for (int i = 10; i >0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
