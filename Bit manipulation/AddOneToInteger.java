3f59f0266c4099273952d18e28423ad3d478e182
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-03 08:44:02 +0530

    solve: add 1 to an integer using bits

---
+++ Bit manipulation/AddOneToInteger.java
@@ -0,0 +1,7 @@
+public class AddOneToInteger {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        System.out.println(a+" + "+1+" will become : "+ (-(~a)));
+    }
+}
