a81fc3773602153d57245cf17cb9f564839b01d3
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-28 08:29:44 +0530

    implement BFS

---
+++ Binary tree/BFS.java
@@ -0,0 +1,75 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+     static class BuildTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node c = q.remove();
+            if (c == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(c.data + "  ");
+                if (c.left != null) {
+                    q.add(c.left);
+                }
+                if (c.right != null) {
+                    q.add(c.right);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BuildTree b = new BuildTree();
+        Node root = b.buildTree(nodes);
+        levelOrder(root);
+    }
+}
