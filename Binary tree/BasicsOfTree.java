45ced6510c80421d45916ce19ac1783fe30d2ce7
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-28 08:41:29 +0530

    solve height of a tree

--- Binary tree/BasicsOfTree.java
+++ Binary tree/BasicsOfTree.java
@@ -29,44 +29,57 @@ public class BasicsOfTree {
             return newNode;
         }
 
-        public static void preOrder(Node root){
-            if(root ==null){
+        public static void preOrder(Node root) {
+            if (root == null) {
                 return;
             }
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
             preOrder(root.left);
             preOrder(root.right);
         }
 
-         public static void postOrder(Node root){
-            if(root ==null){
+        public static void postOrder(Node root) {
+            if (root == null) {
                 return;
             }
             postOrder(root.left);
             postOrder(root.right);
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
         }
 
-         public static void inOrder(Node root){
-            if(root ==null){
+        public static void inOrder(Node root) {
+            if (root == null) {
                 return;
             }
             inOrder(root.left);
-            System.out.print(root.data+" ");
+            System.out.print(root.data + " ");
             inOrder(root.right);
+        }
+
+        public static int height(Node root) {
+            if (root == null) {
+                return 0;
+            }
+            int height = 0;
+            int lH, rH;
+            lH = height(root.left);
+            rH = height(root.right);
+            height = Math.max(lH, rH) + 1;
+            return height;
         }
     }
 
     public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1, 4, 4, -1, 4, 4, -1, 4, 4 };
 
         BuildTree b = new BuildTree();
-        Node root =  b.buildTree(nodes);
+        Node root = b.buildTree(nodes);
 
         b.preOrder(root);
         System.out.println();
         b.postOrder(root);
         System.out.println();
         b.inOrder(root);
+        System.out.println("\nHeight : " + b.height(root));
     }
 }
